96fe7816488ce4f999eb27cb12ff1b6d75ff602c
kferrara <keith_ferrara@yahoo.com>
2012-11-24 08:45:20 -0500

    Removed all files from origianl project.

--- application/config/grocery_crud.php
+++
@@ -1,2 +0,0 @@
-<?php
-	$config['grocery_crud_default_language']	= 'english';
--- public_html/assets/grocery_crud/js/jquery_plugins/config/jquery.chosen.config.js
+++
@@ -1,4 +0,0 @@
-$(function(){
-	$(".chosen-select").chosen({allow_single_deselect:true});
-	$(".chosen-multiple-select").chosen({allow_single_deselect:true});
-});
